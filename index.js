eac83c0b79cdf6f5174fdfa5cdf04a7d9d45fd59
nisit.jiraruwat <nisit.jiraruwat@hot-now.com>
2020-05-14 15:53:54 +0700

    Show example data

--- index.js
+++ index.js
@@ -1,18 +1,64 @@
 const express = require('express')
+const fs = require('fs')
+
+
 const app = express()
 
 const PORT = 3000
 
 app.use('/', express.static('public'))
 
-app.get('/random-numbers', (req, res) => {
-
+app.get('/data/:name', (req, res) => {
+    const fileName = req.params.name
+    fs.readFile(`./data/${fileName}.json`, function(err, data) {
+        if (err) res.status(400).json({ error: err })
+        res.send (JSON.parse(data))
+    });
 })
 
 app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`))
 
 
-const randomNumber = () => {
-    const number = parseInt(Math.random() * 100)
-    return number < 10 ? number + 10 : number;
+const saveData = (fileName, jsonString) => {
+    fs.writeFile(`${fileName}.json`, jsonString, (err) => {
+        if (err) throw err;
+        console.log(`${fileName}.json Saved!`);
+    })
+}
+
+const jsonToSql = (json) => {
+    return json.reduce((sql, table) => {
+        const columnsSql = table.columns.map(column => column.name).join()
+
+        const mapValues = table.values.map(values => {
+            const mapValue = values.map((value, index) => {
+                const column = table.columns[index];
+                if (column.has_ref) {
+                    const ref = json.find(t => t.table_name == column.ref.table)
+                    console.log(ref.columns);
+                    console.log( column.ref.column);
+                    
+                    const refColumnIndex = ref.columns.findIndex(c => c.name == column.ref.column)
+                    console.log(refColumnIndex);
+                    return ref.values[value.ref_row - 1][refColumnIndex].value;
+                } else {
+                    return value.value;
+                }
+            })
+            return `(${mapValue.join()})`
+        })
+
+        return `
+            ${sql}
+            INSERT INTO ${table.table_name} (${columnsSql})
+            VALUES ${mapValues.join()};
+        `
+    }, '')
+}
+
+const saveSql = (fileName, sql) => {
+    fs.writeFile(`${fileName}.sql`, sql, (err) => {
+        if (err) throw err;
+        console.log(`${fileName}.sql Saved!`);
+    })
 }
--- web/src/index.js
+++ web/src/index.js
@@ -1,57 +1,143 @@
-class LikeButton extends React.Component {
+class Main extends React.Component {
   constructor(props) {
-    super(props);
-    this.state = { sql: '', result: '' };
+    super(props)
+    this.state = { data: null, name: location.search.replace('?q=', '') };
   }
 
-  randomNumbers() {
-    axios.post('/random-numbers')
+  componentWillMount() {
+    this.loadData ();
+  }
+
+  loadData() {
+    axios.get(`/data/${this.state.name}`)
     .then(({data}) => {
       console.log(data)
-      this.setState({ sql: data.sql, result: data.result.join() })
+      this.setState({ data })
     })
   }
 
-  getRandomNumbers() {
-    axios.get('/random-numbers2')
-    .then(({data}) => {
-      console.log(data)
-      this.setState({ sql: data.sql, result: data.result.map(({number}) => number).join() })
-    })
-  }
-
-  clearNumbers() {
-    axios.delete('/clear-numbers')
-    .then(({data}) => {
-      console.log(data)
-      this.setState({ sql: data.sql, result: data.result.join() })
-    })
+  updateTable(table, index) {
+    const tables = this.state.data
+    tables[index] = table
+    this.setState({ data: tables })
   }
 
   render() {
     return (
-      <div>
-        <div>
-          <b>SQL:</b><br/>
-          <span>{this.state.sql}</span>
-        </div>
-        <div>
-        <b>Result:</b><br/>
-          <span>{this.state.result}</span>
-        </div>
-        <button onClick={() => this.randomNumbers()}>
-          Random Numbers
-        </button>
-        <button onClick={() => this.getRandomNumbers()}>
-          Get Random Numbers
-        </button>
-        <button onClick={() => this.clearNumbers()}>
-          Clear Numbers
-        </button>
+      <div class="container">
+        {
+          this.state.data
+          ? this.state.data.map ((table, index) => <MyTable
+            table={table}
+            tables={this.state.data}
+            tableIndex={index}
+            updateTable={this.updateTable.bind(this)}/>)
+          : null
+        }
       </div>
     );
   }
 }
 
 const domContainer = document.querySelector('#root');
-ReactDOM.render(React.createElement(LikeButton), domContainer);
+ReactDOM.render(React.createElement(Main), domContainer);
+
+
+class MyTable extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { };
+  }
+
+  handleChange(rowIndex, columnIndex) {
+    return (e) => {
+      const values = this.props.table.values
+      values[rowIndex][columnIndex].value = e.target.value
+      this.props.updateTable ({...this.props.table, values }, this.props.tableIndex)
+    }
+  }
+
+  handleSelectChange(rowIndex, columnIndex) {
+    return (e) => {
+      const values = this.props.table.values
+      values[rowIndex][columnIndex].ref_row = e.target.value
+      this.props.updateTable ({...this.props.table, values }, this.props.tableIndex)
+    }
+  }
+
+  generateHead() {
+    return this.props.table.columns
+      .map (column => <th key={`${this.props.table.table_name}-${column.name}`} ><abbr title={column.name}>{column.name}</abbr></th>)
+  }
+
+  generateBody() {
+    // const refs = this.props.table.columns
+    //   .map (column => {
+    //     console.log(column);
+        
+    //     if (column.has_ref) {
+    //       const ref = this.props.tables.find(t => t.table_name == column.ref.table)
+    //       const columnIndex = ref.columns.findIndex(c => c.name == column.ref.column)
+    //       return ref.values.map (values => values[columnIndex].value)
+    //     } else {
+    //       return null
+    //     }
+    //   })
+  
+    return this.props.table.values
+      .map ((values, rowIndex) => {
+        return <tr key={`${this.props.table.table_name}-tr-${rowIndex}`}>
+          {
+            values.map ((value, index) => {
+              if (this.props.table.columns[index].has_ref) {
+                const column = this.props.table.columns[index]
+                const ref = this.props.tables.find(t => t.table_name == column.ref.table)
+                const refColumnIndex = ref.columns.findIndex(c => c.name == column.ref.column)
+
+                return <td key={`${this.props.table.table_name}-td-${rowIndex}-${index}`}>
+                  {this.generateRefDropdown(ref, rowIndex, index, refColumnIndex)}
+                  {ref.values[value.ref_row][refColumnIndex].value}
+                </td>
+              } else {
+                return <td key={`${this.props.table.table_name}-td-${rowIndex}-${index}`}>
+                  <input class="input" type="text" value={value.value} onChange={this.handleChange(rowIndex, index)}></input>
+                </td>
+              }
+            })
+          }
+        </tr>
+      })
+  }
+
+  generateRefDropdown(ref, rowIndex, columnIndex, refColumnIndex) {
+    return <div class="control" key={`${this.props.table.table_name}-option-${rowIndex}-${columnIndex}-${refColumnIndex}`}>
+        <div class="select">
+          <select value={this.props.value} onChange={this.handleSelectChange(rowIndex, columnIndex)}>
+            { 
+              ref.values.map ((_, index) =>
+            <option key={`${this.props.table.table_name}-option-${rowIndex}-${columnIndex}-${refColumnIndex}-${index}`} value={index}>{`${ref.table_name}(${index})`}</option>
+              )
+            }
+          </select>
+        </div>
+    </div>
+  }
+
+  render() {
+    return (
+      <section style={{marginBottom: "16px"}}>
+        <div><span class="tag is-primary">{this.props.table.table_name}</span></div>
+        <table class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth">
+          <thead>
+            <tr>
+              {this.generateHead()}
+            </tr>
+          </thead>
+          <tbody>
+            {this.generateBody()}
+          </tbody>
+        </table>
+      </section>
+    );
+  }
+}
