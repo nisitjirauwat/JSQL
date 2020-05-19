63621da98ed99ac159db0c4ce92b97f03bfa9590
nisit.jiraruwat <nisit.jiraruwat@hot-now.com>
2020-05-19 19:40:11 +0700

    Delete sql

--- index.js
+++ index.js
@@ -46,11 +46,18 @@ app.post('/data/:name', (req, res) => {
                 fs.writeFile(`./sql/${dir}/${service}.sql`, generateSql(req.body.tables, service), (err) => {
                     if (err && !hasErr) {
                         res.status(400).json({ error: err })
-                        return true
+                        hasErr = true
                     }
-
-                    return hasErr
                 })
+
+                fs.writeFile(`./sql/${dir}/delete-${service}.sql`, generateDeleteSql(req.body.tables, service), (err) => {
+                    if (err && !hasErr) {
+                        res.status(400).json({ error: err })
+                        hasErr = true
+                    }
+                })
+
+                return hasErr
             }, false)
 
             if (!hasErr) {
@@ -100,4 +107,39 @@ const findServices = (json) => {
         }
         return services
     }, [])
+}
+
+const generateDeleteSql = (json, service) => {
+    return json.filter(table => table.service == service).reverse().reduce((sql, table) => {
+        if (table.values.length == 0) {
+            return sql
+        }
+
+        const deleteValues = table.values.map((values) => {
+            const deleteValues = values.map((value, index) => {
+                const column = table.columns[index]
+                if (column.primary_key) {
+                    let v = value.value
+
+                    if (column.has_ref) {
+                        const ref = json.find(t => t.table_name == column.ref.table && t.service == column.ref.service)
+                        const refColumnIndex = ref.columns.findIndex(c => c.name == column.ref.column)
+                        v = ref.values[value.ref_row][refColumnIndex].value
+                    }
+
+                    return `${column.name}=${v}`
+                } else {
+                    return null
+                }
+            }, `DELETE FROM ${table.table_name} WHERE`)
+
+            deleteValueSql = deleteValues.filter(deleteValue => deleteValue != null).join(" AND ")
+            return `DELETE FROM ${table.table_name} WHERE ${deleteValueSql};\n`
+        })
+
+        return `
+            ${sql}
+            ${deleteValues.join("\n")}
+        `
+    }, '')
 }
